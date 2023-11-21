3eed27c317d59989f34c60b7ec247cfce5634d59
mohanson <mohanson@outlook.com>
2023-11-21 10:15:33 +0800

    Add transfer spore demo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import { accounts } from "./wallet";
+
 console.log(`Main: path on ${__dirname}`)
+console.log(`Main: alice.address = ${accounts.alice.address}`)
+console.log(`Main:   bob.address = ${accounts.bob.address}`)
